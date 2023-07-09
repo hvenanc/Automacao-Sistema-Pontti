b3f9bbf67143c7666b0c630e8e03fb9415f7d475
hvenanc <hvs@poli.br>
2023-07-09 20:47:21 -0300

    Login Simples CTE e Automação da data dos Sábados + .env

--- simples_cte.py
+++ simples_cte.py
@@ -2,14 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from ambiente import get_variable
 
 driver = webdriver.Firefox()
 driver.get("https://app.simplescte.com.br/login")
 assert "Simples" in driver.title
 elem = driver.find_element(By.CSS_SELECTOR, "#\:r0\:")
-elem.clear()
-elem.send_keys("faturamentojvsantostransportes@gmail.com")
+elem.clear(get_variable('USUARIO_SCTE'))
+elem.send_keys()
 elem = driver.find_element(By.CSS_SELECTOR, "#\:r1\:")
 elem.clear()
-elem.send_keys("agr@2019")
+elem.send_keys(get_variable('SENHA_SCTE'))
 driver.find_element(By.CSS_SELECTOR,"button.MuiButtonBase-root:nth-child(1)").click()
